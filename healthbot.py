59f01a6502526a8d6b2160e314493fd6f5d8c43f
isabel <isabelbocolo5@gmail.com>
2023-12-09 21:30:14 +0200

    Created json file with custom data

--- healthbot.py
+++ healthbot.py
@@ -1,11 +1,29 @@
 from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+from chatterbot.trainers import ListTrainer
+import json
 
-chatbot = ChatBot("Chatpot")
 
-exit_conditions = (":q", "quit", "exit")
+chatbot = ChatBot("Thulani")
+# trainer = ChatterBotCorpusTrainer(chatbot)  
+trainer = ListTrainer(chatbot)
+
+with open("./covid_faq.json", "r", encoding="utf-8") as json_file:
+    data = json.load(json_file)
+
+for question, answer in data.items():
+    trainer.train([question, answer])
+
+print("****\nHi! My name is Thulani and you can ask me any questions regarding health, whilst I can provide information, it is important to note I am not a substitue for a medical practitioner.\n****")
+
+exit_conditions = ("bye", "quit", "exit")
 while True:
-    query = input("> ")
-    if query in exit_conditions:
+    try:
+        query = input("User: ")
+        if query in exit_conditions:
+            break
+        else:
+            print(f"Dr Thulani:  {chatbot.get_response(query)}")
+
+    except(KeyboardInterrupt, EOFError, SystemExit):
         break
-    else:
-        print(f"Dr Thulani:  {chatbot.get_response(query)}")
